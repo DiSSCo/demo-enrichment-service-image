29b00d80d51fc4839589fa43eff334198feb33ad
southeo <soulaine.theo@gmail.com>
2023-11-15 15:30:00 +0100

    Feature/update plant organ detection (#25)
    * Map to annotation model
    
    * sonar
    
    * Code review
    
    * Code review

--- herbarium-sheet-plant-organ-detection/main.py
+++ herbarium-sheet-plant-organ-detection/main.py
@@ -2,7 +2,7 @@ import json
 import logging
 import os
 
-import requests as requests
+import requests
 from kafka import KafkaConsumer, KafkaProducer
 from PIL import Image
 import numpy as np
@@ -11,10 +11,12 @@ from datetime import datetime, timezone
 from detectron2.config import get_cfg
 from detectron2.engine.defaults import DefaultPredictor
 from detectron2 import model_zoo
-from detectron2.data.detection_utils import convert_PIL_to_numpy
+from typing import Tuple
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
+ODS_TYPE = 'ods:type'
+ODS_ID = 'ods:id'
 
 def start_kafka(predictor: DefaultPredictor) -> None:
     """
@@ -22,67 +24,91 @@ def start_kafka(predictor: DefaultPredictor) -> None:
     When done it will republish the object, so it can be validated and storage by the processing service
     :param name: The topic name the Kafka listener needs to listen to
     """
-    consumer = KafkaConsumer(os.environ.get('KAFKA_CONSUMER_TOPIC'), group_id=os.environ.get('KAFKA_CONSUMER_GROUP'),
-                             bootstrap_servers=[os.environ.get('KAFKA_CONSUMER_HOST')],
-                             value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+    consumer = KafkaConsumer(os.environ.get('KAFKA_CONSUMER_TOPIC'),
+                             group_id=os.environ.get('KAFKA_CONSUMER_GROUP'),
+                             bootstrap_servers=[
+                                 os.environ.get('KAFKA_CONSUMER_HOST')],
+                             value_deserializer=lambda m: json.loads(
+                                 m.decode('utf-8')),
                              enable_auto_commit=True)
-    producer = KafkaProducer(bootstrap_servers=[os.environ.get('KAFKA_PRODUCER_HOST')],
-                             value_serializer=lambda m: json.dumps(m).encode('utf-8'))
+    producer = KafkaProducer(
+        bootstrap_servers=[os.environ.get('KAFKA_PRODUCER_HOST')],
+        value_serializer=lambda m: json.dumps(m).encode('utf-8'))
     for msg in consumer:
         logging.info(msg.value)
         json_value = msg.value
-        image_uri = json_value['data']['ac:accessURI']
-        additional_info_annotations = run_object_detection(image_uri, predictor)
+        image_uri = json_value['object']['digitalEntity']['ac:accessURI']
+        additional_info_annotations, width, height = run_object_detection(image_uri, predictor)
         logging.info('Publishing the result: %s', json_value)
-        annotation = map_to_annotation(json_value, additional_info_annotations)
-        send_updated_opends(annotation, producer)
+        event = map_to_annotation(json_value, additional_info_annotations, width, height)
+        event["jobId"] = json_value['jobId']
+        send_updated_opends(event, producer)
 
 
-def map_to_annotation(json_value, additional_info_annotations) -> dict:
-    values = []
+def map_to_annotation(json_value, additional_info_annotations, width, height) -> dict:
+    annotations = []
+    timestamp = timestamp_now()
     for value in additional_info_annotations:
-        values.append(
-            {
-                'class': value['class'],
-                'score': value['score'],
-                'selector': {
-                    'type': 'FragmentSelector',
-                    'conformsTo': 'http://www.w3.org/TR/media-frags/',
-                    'value': "xywh=" + str(value['boundingBox'][0]) + ',' + str(value['boundingBox'][1]) + ',' + str(
-                        value['boundingBox'][2]) + ',' + str(value['boundingBox'][3])
+        body_values = [{
+            'class': value['class'],
+            'score': value['score'],
+        }]
+        selector = {
+            'oa:selector': {
+                ODS_TYPE: 'FragmentSelector',
+                'dcterms:conformsTo': 'https://www.w3.org/TR/media-frags/',
+                'ac:hasRoi': {
+                    "ac:xFrac": value['boundingBox'][0] / width,
+                    "ac:yFrac": value['boundingBox'][1] / height,
+                    "ac:widthFrac": (value['boundingBox'][2] -
+                                     value['boundingBox'][0]) / width,
+                    "ac:heightFrac": (value['boundingBox'][3]
+                                      - value['boundingBox'][1]) / height
                 }
             }
-        )
-    annotation = {
-        'type': 'Annotation',
-        'motivation': 'https://hdl.handle.net/pid-machine-enrichment',
-        'creator': 'https://hdl.handle.net/enrichment-service-pid',
-        'created': timestamp_now(),
-        'target': {
-            'id': json_value['id'],
-            'type': 'https://hdl.handle.net/21...'
-        },
-        'body': {
-            'source': json_value['data']['ac:accessURI'],
-            'purpose': 'describing',
-            'values': values
         }
+        annotation = {
+            'rdf:type': 'Annotation',
+            'oa:motivation': 'ods:adding',
+            'oa:creator': {
+                ODS_TYPE: 'machine',
+                'foaf:name': os.environ.get('MAS_NAME'),
+                ODS_ID: os.environ.get('MAS_ID')
+            },
+            'dcterms:created': timestamp,
+            'oa:target': {
+                ODS_ID: json_value['object']['digitalEntity'][ODS_ID],
+                ODS_TYPE: json_value['object']['digitalEntity'][ODS_TYPE],
+                'oa:selector': selector
+            },
+            'oa:body': {
+                ODS_TYPE: 'TextualBody',
+                'dcterms:reference': 'https://github.com/2younis/plant-organ'
+                                     '-detection/blob/master/train_net.py',
+                'oa:value': body_values
+            }
+        }
+        annotations.append(annotation)
+    logging.info(annotations)
+    event = {
+        'annotations': annotations
     }
-    logging.info(annotation)
-    return annotation
+    return event
 
 
 def timestamp_now():
-    timestamp = str(datetime.now(tz=timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f"))
+    timestamp = str(datetime.now(tz=timezone.utc).strftime("%Y-%m-%dT%H:%M:%S"
+                                                           ".%f"))
     timestamp_cleaned = timestamp[:-3]
     timestamp_timezone = timestamp_cleaned + 'Z'
     return timestamp_timezone
+
 
 def send_updated_opends(annotation: dict, producer: KafkaProducer) -> None:
     producer.send('annotation', annotation)
 
 
-def run_object_detection(image_uri: str, predictor: DefaultPredictor) -> list:
+def run_object_detection(image_uri: str, predictor: DefaultPredictor) -> Tuple[list, int, int]:
     """
     Checks if the Image url works and gathers metadata information from the image
     :param image_uri: The image url from which we will gather metadata
@@ -90,6 +116,7 @@ def run_object_detection(image_uri: str, predictor: DefaultPredictor) -> list:
     """
     try:
         img = Image.open(requests.get(image_uri, stream=True).raw)
+        width, height = img.size
         predictions = predictor(np.array(img))
         instances = predictions['instances']
         annotations_result = []
@@ -111,10 +138,10 @@ def run_object_detection(image_uri: str, predictor: DefaultPredictor) -> list:
                 'boundingBox': [int(x) for x in boxes[i]]
             })
 
-        return annotations_result
+        return annotations_result, width, height
     except FileNotFoundError:
         logging.exception('Failed to retrieve picture')
-        return []
+        return [], -1, -1
 
 
 if __name__ == '__main__':
@@ -125,4 +152,5 @@ if __name__ == '__main__':
     predictor = DefaultPredictor(cfg)
 
     start_kafka(predictor)
-    # run_object_detection('https://www.unimus.no/felles/bilder/web_hent_bilde.php?id=14894911&type=jpeg', predictor)
+    # annotation, width, height = run_object_detection('https://www.unimus.no/felles/bilder/web_hent_bilde.php?id=14894911&type=jpeg', predictor)
+    # map_to_annotation(None, annotation, width, height), indent=2))
